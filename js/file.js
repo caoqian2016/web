79a1113b8c5ecfc7c5bf7730c9f0c7ab5eebc38a
Caoqian <caoqian14@126.com>
2016-10-18 14:04:17 +0800

    chapter7

--- js/file.js
+++ js/file.js
@@ -29,6 +29,34 @@ function showpic(whichpic){
 	return true;
 }
 
+function insertAfter(newElement,targetElement){
+	var parent=targetElement.parentNode;
+	if (parent.lastChild==targetElement){
+		parent.appendChild(newElement);
+	}
+	else{
+		parent.insertBefore(newElement,targetElement.nextSibling);
+	}
+}
+
+function preparePlaceholder(){
+	if (!document.createElement) return false;
+	if (!document.createTextNode) return false;
+	if (!document.getElementById) return false;
+	if (!document.getElementById("imagegallery")) return false;
+	var placeholder=document.createElement("img");
+	placeholder.setAttribute("id","placeholder");
+	placeholder.setAttribute("src","image/a.jpg");
+	placeholder.setAttribute("alt","placeholder");
+	var description=document.createElement("p");
+	description.setAttribute("id","description");
+	var desctext=document.createTextNode("选择一幅图片");
+	description.appendChild(desctext);
+	var gallery=document.getElementById("imagegallery");
+	insertAfter(placeholder,gallery);
+	insertAfter(description,placeholder);
+}
+
 //辅助函数，用于为页面添加初始化函数
 function addLoadEvent(func) { 
   var oldonload = window.onload; 
@@ -43,5 +71,5 @@ function addLoadEvent(func) {
 } 
 
 // 执行此添加
+addLoadEvent(preparePlaceholder);
 addLoadEvent(prepareGallery);
-
